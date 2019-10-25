7e966f59fff05cbfabc452399ce0b0a135456f69
Pancho Labrador <plabradorjr@gmail.com>
2019-10-25 06:29:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,7 +36,7 @@ class Song
 
   def self.find_or_create_by_name(name)
     if self.find_by_name(name) == true
-      self.find_by_name(name)
+      self.name 
     else
       self.create_by_name(name)
     end
